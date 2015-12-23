c8618e0069d29290a8f2d7aa3aa18a96b969b21a
Sutanu Dalui <sutanu.dalui@ericsson.com>
2015-12-23 18:23:32 +0530

    copyright notice

--- src/main/java/com/underthehood/weblogs/config/CassandraConfiguration.java
+++ src/main/java/com/underthehood/weblogs/config/CassandraConfiguration.java
@@ -6,10 +6,10 @@
  * See class description
  *
  * Copyright (C) 2015 by
- * ERICSSON
+ * 
  *
  * The program may be used and/or copied only with the written
- * permission from Ericsson Inc, or in accordance with
+ * permission from  or in accordance with
  * the terms and conditions stipulated in the agreement/contract
  * under which the program has been supplied.
  *
